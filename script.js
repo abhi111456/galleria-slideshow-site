174b918bae59f1c1ec0c0339e5e3a43e03ac2b37
abhi111456 <abhishek.21jics039@jietjodhpur.ac.in>
2024-06-30 20:08:33 +0530

    a

--- script.js
+++ script.js
@@ -1,43 +1,25 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const grid = document.getElementById('grid');
-    const paintingsStorage = window.sessionStorage;
 
-    const displayData = (data) => {
-        console.log('Data received:', data); // Debugging line
-        data.forEach((element, index) => {
-            grid.innerHTML += `
-            <section class="image-wrapper grid-item">
-                <a class="grid-item__link" href="detail.html?painting=${index}" data-id="${element.id}">
-                    <div class="grid-item__text">
-                        <h2 class="heading heading--2 white">${element.alt_description || 'Untitled'}</h2>
-                        <p class="subhead subhead--2 white--opacity">${element.user.name}</p>
-                    </div>
-                    <img class="grid-item__image" src="${element.urls.small}" alt="${element.alt_description || 'Image'}">
-                </a>
-            </section>`;
-            paintingsStorage.setItem(index, JSON.stringify(element));
+
+const accessKey = 'LlUYQn6NuuAWvMZ1IC1gAXfIZjFVAOVGRNPJf3oI4Zc';
+
+const imageList = document.getElementById('image-list');
+
+fetch(`https://api.unsplash.com/photos?client_id=${accessKey}`)
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(item => {
+            const imageListItem = document.createElement('div');
+            imageListItem.classList.add('image-list-item');
+
+            const img = document.createElement('img');
+            img.src = `${item.urls.regular}`;
+            img.alt = item.alt_description;
+            img.loading = 'lazy';
+
+            imageListItem.appendChild(img);
+            imageList.appendChild(imageListItem);
         });
-
-        Macy({
-            container: '.grid',
-            columns: 4,
-            margin: 40,
-            breakAt: {
-                1100: 3,
-                840: 2,
-                600: 1,
-            },
-            waitForImages: true,
-        });
-    };
-
-    fetch('https://api.unsplash.com/photos/?client_id=LlUYQn6NuuAWvMZ1IC1gAXfIZjFVAOVGRNPJf3oI4Zc')
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok ' + response.statusText);
-            }
-            return response.json();
-        })
-        .then(data => displayData(data))
-        .catch(error => console.error('Error fetching data:', error));
-});
+    })
+    .catch(error => {
+        console.error('Error fetching the images:', error);
+    });
